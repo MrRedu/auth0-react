ed6ce781372e1ddf9c39251d0737204a4734d0c7
Eduardo <ejrb1234@gmail.com>
2023-11-24 17:57:26 -0400

    added Auth0 package

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={import.meta.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={import.meta.env.REACT_APP_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
